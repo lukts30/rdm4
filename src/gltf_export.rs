6068e5df4d44ecc1ca7d58a211ac6789b1c41f5b
lukts30 <llukas21307@gmail.com>
2020-04-11 21:21:46 +0200

    initial commit

---
+++ src/gltf_export.rs
@@ -0,0 +1,206 @@
+use gltf::json;
+
+use std::{fs, mem};
+
+use json::validation::Checked::Valid;
+
+use std::io::Write;
+
+
+
+use crate::RDModell;
+use crate::Triangle;
+use crate::VertexFormat;
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+fn to_padded_byte_vector<T>(vec: Vec<T>) -> Vec<u8> {
+    let byte_length = vec.len() * mem::size_of::<T>();
+    let byte_capacity = vec.capacity() * mem::size_of::<T>();
+    let alloc = vec.into_boxed_slice();
+    let ptr = Box::<[T]>::into_raw(alloc) as *mut u8;
+    let mut new_vec = unsafe { Vec::from_raw_parts(ptr, byte_length, byte_capacity) };
+    while new_vec.len() % 4 != 0 {
+        new_vec.push(0); // pad to multiple of four bytes
+    }
+    new_vec
+}
+
+fn rdm_vertex_to_gltf(input_vec: Vec<VertexFormat>) -> (Vec<Vertex>, Vec<f32>, Vec<f32>) {
+    let mut out: Vec<Vertex> = Vec::new();
+
+    let mut min: Vec<f32> = vec![100.0,100.0,100.0];
+    let mut max: Vec<f32> = vec![-100.0,-100.0,-100.0];
+
+    for vert in input_vec {
+        let p4h = vert.get_p4h();
+        let x = p4h.pos[0].to_f32();
+        let y = p4h.pos[1].to_f32();
+        let z = p4h.pos[2].to_f32();
+
+        min[0] = x.min(min[0]);
+        min[1] = y.min(min[1]);
+        min[2] = z.min(min[2]);
+
+        max[0] = x.max(max[0]);
+        max[1] = y.max(max[1]);
+        max[2] = z.max(max[2]);
+
+        let t = Vertex {
+            position: [x, y, z],
+        };
+        out.push(t);
+    }
+    (out, min, max)
+}
+
+pub fn export(rdm: RDModell) {
+    let conv = rdm_vertex_to_gltf(rdm.vertices);
+
+    let triangle_vertices = conv.0;
+    let min = conv.1;
+    let max = conv.2;
+
+    let triangle_idx: Vec<Triangle> = rdm.triangle_indices;
+
+    let buffer_length = (triangle_vertices.len() * mem::size_of::<Vertex>()) as u32;
+    let buffer = json::Buffer {
+        byte_length: buffer_length,
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        uri: Some("buffer0.bin".into()),
+    };
+    let buffer_view = json::buffer::View {
+        buffer: json::Index::new(0),
+        byte_length: buffer.byte_length,
+        byte_offset: None,
+        byte_stride: None,
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        target: Some(Valid(json::buffer::Target::ArrayBuffer)),
+    };
+    let positions = json::Accessor {
+        buffer_view: Some(json::Index::new(0)),
+        byte_offset: 0,
+        count: triangle_vertices.len() as u32,
+        component_type: Valid(json::accessor::GenericComponentType(
+            json::accessor::ComponentType::F32,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Valid(json::accessor::Type::Vec3),
+        min: Some(json::Value::from(min)),
+        max: Some(json::Value::from(max)),
+        name: None,
+        normalized: false,
+        sparse: None,
+    };
+
+    let triangle_idx_len_b = (triangle_idx.len() * mem::size_of::<Triangle>()) as u32;
+
+    let buffer_idx = json::Buffer {
+        byte_length: triangle_idx_len_b,
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        uri: Some("buffer1.bin".into()),
+    };
+    let buffer_idx_view = json::buffer::View {
+        buffer: json::Index::new(1),
+        byte_length: triangle_idx_len_b,
+        byte_offset: None,
+        byte_stride: None,
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        target: Some(Valid(json::buffer::Target::ElementArrayBuffer)),
+    };
+
+    let idx = json::Accessor {
+        buffer_view: Some(json::Index::new(1)),
+        byte_offset: 0,
+        count: (triangle_idx.len() * 3) as u32,
+        component_type: Valid(json::accessor::GenericComponentType(
+            json::accessor::ComponentType::U16,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Valid(json::accessor::Type::Scalar),
+        min: None,
+        max: None,
+        name: None,
+        normalized: false,
+        sparse: None,
+    };
+
+    let primitive = json::mesh::Primitive {
+        attributes: {
+            let mut map = std::collections::HashMap::new();
+            map.insert(Valid(json::mesh::Semantic::Positions), json::Index::new(0));
+            map
+        },
+        extensions: Default::default(),
+        extras: Default::default(),
+        indices: Some(json::Index::new(1)),
+        material: None,
+        mode: Valid(json::mesh::Mode::Triangles),
+        targets: None,
+    };
+
+    let mesh = json::Mesh {
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        primitives: vec![primitive],
+        weights: None,
+    };
+
+    let node = json::Node {
+        camera: None,
+        children: None,
+        extensions: Default::default(),
+        extras: Default::default(),
+        matrix: None,
+        mesh: Some(json::Index::new(0)),
+        name: None,
+        rotation: None,
+        scale: None,
+        translation: None,
+        skin: None,
+        weights: None,
+    };
+
+    let root = json::Root {
+        accessors: vec![positions, idx],
+        buffers: vec![buffer, buffer_idx],
+        buffer_views: vec![buffer_view, buffer_idx_view],
+        meshes: vec![mesh],
+        nodes: vec![node],
+        scenes: vec![json::Scene {
+            extensions: Default::default(),
+            extras: Default::default(),
+            name: None,
+            nodes: vec![json::Index::new(0)],
+        }],
+        ..Default::default()
+    };
+
+    let _ = fs::create_dir("triangle");
+
+    let writer = fs::File::create("triangle/triangle.gltf").expect("I/O error");
+    json::serialize::to_writer_pretty(writer, &root).expect("Serialization error");
+
+    let bin = to_padded_byte_vector(triangle_vertices);
+    let mut writer = fs::File::create("triangle/buffer0.bin").expect("I/O error");
+    writer.write_all(&bin).expect("I/O error");
+
+    let bin2 = to_padded_byte_vector(triangle_idx);
+    let mut writer2 = fs::File::create("triangle/buffer1.bin").expect("I/O error");
+    writer2.write_all(&bin2).expect("I/O error");
+}
---
+++ src/main.rs
@@ -0,0 +1,432 @@
+use bytes::{Buf, Bytes};
+use std::path::Path;
+
+use std::fs::File;
+use std::io;
+
+use half::f16;
+use std::fmt;
+
+#[macro_use]
+extern crate log;
+
+mod gltf_export;
+
+pub struct RDModell {
+    buffer: Bytes,
+    vertices: Vec<VertexFormat>,
+    triangle_indices: Vec<Triangle>,
+
+    meta: u32,
+    vertex_offset: u32,
+    vertices_count: u32,
+    vertex_buffer_size: u32,
+
+    triangles_offset: u32,
+    triangles_idx_count: u32,
+    triangles_idx_size: u32,
+}
+
+trait GetVertex {
+    fn get_p4h(&mut self) -> P4h;
+    fn get_n4b(&mut self) -> N4b;
+    fn get_g4b(&mut self) -> G4b;
+    fn get_b4b(&mut self) -> B4b;
+    fn get_t2h(&mut self) -> T2h;
+    fn get_i4b(&mut self) -> I4b;
+    fn get_c4c(&mut self) -> C4c;
+}
+
+impl GetVertex for Bytes {
+    fn get_p4h(&mut self) -> P4h {
+        let p4h = P4h {
+            pos: [
+                f16::from_bits(self.get_u16_le()),
+                f16::from_bits(self.get_u16_le()),
+                f16::from_bits(self.get_u16_le()),
+                f16::from_bits(self.get_u16_le()),
+            ],
+        };
+        p4h
+    }
+    fn get_n4b(&mut self) -> N4b {
+        let n4b = N4b {
+            normals: [self.get_u8(), self.get_u8(), self.get_u8(), self.get_u8()],
+        };
+        n4b
+    }
+    fn get_g4b(&mut self) -> G4b {
+        let g4b = G4b {
+            tangent: [self.get_u8(), self.get_u8(), self.get_u8(), self.get_u8()],
+        };
+        g4b
+    }
+    fn get_b4b(&mut self) -> B4b {
+        let b4b = B4b {
+            binormal: [self.get_u8(), self.get_u8(), self.get_u8(), self.get_u8()],
+        };
+        b4b
+    }
+    fn get_t2h(&mut self) -> T2h {
+        let t2h = T2h {
+            tex: [
+                f16::from_bits(self.get_u16_le()),
+                f16::from_bits(self.get_u16_le()),
+            ],
+        };
+        t2h
+    }
+    fn get_i4b(&mut self) -> I4b {
+        let i4b = I4b {
+            blend_idx: [self.get_u8(), self.get_u8(), self.get_u8(), self.get_u8()],
+        };
+        i4b
+    }
+
+    fn get_c4c(&mut self) -> C4c {
+        let c4c = C4c {
+            unknown: [self.get_u8(), self.get_u8(), self.get_u8(), self.get_u8()],
+        };
+        c4c
+    }
+}
+
+impl RDModell {
+    const META_OFFSET: u32 = 32;
+    const META_COUNT: u32 = 8; //neg
+    const META_SIZE: u32 = 4; //neg
+    const VERTEX_META: u32 = 12;
+    const TRIANGLES_META: u32 = 16;
+
+    fn new(buf: Vec<u8>) -> Self {
+        let size = buf.len() as u32;
+        let buffer = Bytes::from(buf);
+        let mut nbuffer = buffer.clone();
+
+        nbuffer.advance(RDModell::META_OFFSET as usize);
+        let meta = nbuffer.get_u32_le();
+
+        nbuffer.advance((meta - (size - nbuffer.remaining() as u32)) as usize);
+        nbuffer.advance(RDModell::VERTEX_META as usize);
+        let vertex_offset = nbuffer.get_u32_le();
+
+        let triangles_offset = nbuffer.get_u32_le();
+
+        let vertex_count_off = vertex_offset - RDModell::META_COUNT;
+        info!("off : {}", vertex_count_off);
+        nbuffer.advance((vertex_count_off - (size - nbuffer.remaining() as u32)) as usize);
+        let vertices_count = nbuffer.get_u32_le();
+        let vertex_buffer_size = nbuffer.get_u32_le();
+
+        let mut vert_read_buf = nbuffer.clone();
+
+        let vertices_vec: Option<Vec<VertexFormat>> = match vertex_buffer_size {
+
+            VertexFormatSize::P4h => {                
+                vert_read_buf.truncate((vertices_count * vertex_buffer_size) as usize);
+                assert_eq!(vert_read_buf.remaining() % vertex_buffer_size as usize, 0);
+                let mut verts_vec: Vec<VertexFormat> = Vec::with_capacity(vertices_count as usize);
+
+                for _ in 0..vertices_count {
+                    let p4h = vert_read_buf.get_p4h();
+                    let k = VertexFormat::P4h(p4h);
+                    verts_vec.push(k);
+                }
+                assert_eq!(verts_vec.len(), vertices_count as usize);
+                assert_eq!(vert_read_buf.is_empty(), true);
+                info!(
+                    "Read {} vertices of type P4h ({} bytes)",
+                    verts_vec.len(),vertex_buffer_size
+                );
+                Some(verts_vec)
+            }
+
+            VertexFormatSize::P4h_N4b_T2h_C4c => {                
+                vert_read_buf.truncate((vertices_count * vertex_buffer_size) as usize);
+                assert_eq!(vert_read_buf.remaining() % vertex_buffer_size as usize, 0);
+                let mut verts_vec: Vec<VertexFormat> = Vec::with_capacity(vertices_count as usize);
+
+                for _ in 0..vertices_count {
+                    let p4h = vert_read_buf.get_p4h();
+                    let n4b = vert_read_buf.get_n4b();
+                    let t2h = vert_read_buf.get_t2h();
+                    let c4c = vert_read_buf.get_c4c();
+
+                    let k = VertexFormat::P4h_N4b_T2h_C4c(p4h, n4b, t2h,c4c);
+                    verts_vec.push(k);
+                }
+                assert_eq!(verts_vec.len(), vertices_count as usize);
+                assert_eq!(vert_read_buf.is_empty(), true);
+                info!(
+                    "Read {} vertices of type P4h_N4b_T2h_C4c ({} bytes)",
+                    verts_vec.len(),vertex_buffer_size
+                );
+                Some(verts_vec)
+            }
+
+            VertexFormatSize::P4h_N4b_G4b_B4b_T2h => {                
+                vert_read_buf.truncate((vertices_count * vertex_buffer_size) as usize);
+                assert_eq!(vert_read_buf.remaining() % vertex_buffer_size as usize, 0);
+                let mut verts_vec: Vec<VertexFormat> = Vec::with_capacity(vertices_count as usize);
+
+                for _ in 0..vertices_count {
+                    let p4h = vert_read_buf.get_p4h();
+                    let n4b = vert_read_buf.get_n4b();
+                    let g4b = vert_read_buf.get_g4b();
+                    let b4b = vert_read_buf.get_b4b();
+                    let t2h = vert_read_buf.get_t2h();
+                    let k = VertexFormat::P4h_N4b_G4b_B4b_T2h(p4h, n4b, g4b, b4b, t2h);
+                    verts_vec.push(k);
+                }
+                assert_eq!(verts_vec.len(), vertices_count as usize);
+                assert_eq!(vert_read_buf.is_empty(), true);
+                info!(
+                    "Read {} vertices of type P4h_N4b_G4b_B4b_T2h ({} bytes)",
+                    verts_vec.len(),vertex_buffer_size
+                );
+                Some(verts_vec)
+            }
+
+            VertexFormatSize::P4h_N4b_G4b_B4b_T2h_I4b => {                
+                vert_read_buf.truncate((vertices_count * vertex_buffer_size) as usize);
+                assert_eq!(vert_read_buf.remaining() % vertex_buffer_size as usize, 0);
+                let mut verts_vec: Vec<VertexFormat> = Vec::with_capacity(vertices_count as usize);
+
+                for _ in 0..vertices_count {
+                    let p4h = vert_read_buf.get_p4h();
+                    let n4b = vert_read_buf.get_n4b();
+                    let g4b = vert_read_buf.get_g4b();
+                    let b4b = vert_read_buf.get_b4b();
+                    let t2h = vert_read_buf.get_t2h();
+                    let i4b = vert_read_buf.get_i4b();
+                    let k = VertexFormat::P4h_N4b_G4b_B4b_T2h_I4b(p4h, n4b, g4b, b4b, t2h, i4b);
+                    verts_vec.push(k);
+                }
+                assert_eq!(verts_vec.len(), vertices_count as usize);
+                assert_eq!(vert_read_buf.is_empty(), true);
+                info!(
+                    "Read {} vertices of type P4h_N4b_G4b_B4b_T2h_I4b ({} bytes)",
+                    verts_vec.len(),vertex_buffer_size
+                );
+                Some(verts_vec)
+            }
+            _ => {
+                error!("vertices use unrecognised size of {}", vertex_buffer_size);
+                None
+            }
+        };
+
+        let triangles_count_off = triangles_offset - RDModell::META_COUNT;
+        nbuffer.advance((triangles_count_off - (size - nbuffer.remaining() as u32)) as usize);
+        let triangles_idx_count = nbuffer.get_u32_le();
+        let triangles_idx_size = nbuffer.get_u32_le();
+
+        // read indices for triangles 
+        assert_eq!(triangles_idx_size,2);
+        assert_eq!(triangles_idx_count%3,0);
+        let mut triangles_idx_buffer = nbuffer.clone();
+        triangles_idx_buffer.truncate((triangles_idx_size*triangles_idx_count) as usize);
+        let triangles_real_count = triangles_idx_count/3;
+        let mut triangles = Vec::with_capacity(triangles_real_count as usize);
+        for _ in 0..triangles_real_count {
+            let t = Triangle {
+                indices: [
+                    triangles_idx_buffer.get_u16_le(),
+                    triangles_idx_buffer.get_u16_le(),
+                    triangles_idx_buffer.get_u16_le()
+                ]
+            };
+            triangles.push(t);
+        }
+
+
+        let modell = RDModell {
+            buffer: buffer,
+            vertices: vertices_vec.unwrap(),
+            triangle_indices: triangles,
+            meta: meta,
+            vertex_offset: vertex_offset,
+            vertices_count: vertices_count,
+            vertex_buffer_size: vertex_buffer_size,
+
+            triangles_offset: triangles_offset,
+            triangles_idx_count: triangles_idx_count,
+            triangles_idx_size: triangles_idx_size,
+        };
+        modell
+    }
+}
+
+impl fmt::Debug for RDModell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let felm = self.vertices.first().unwrap();
+        let vformat = match felm {
+            VertexFormat::P4h(_) => "P4h",
+            VertexFormat::P4h_N4b_T2h_C4c(_,_,_,_) => "P4h_N4b_T2h_C4c",
+            VertexFormat::P4h_N4b_G4b_B4b_T2h(_,_,_,_,_) => "P4h_N4b_G4b_B4b_T2h",
+            VertexFormat::P4h_N4b_G4b_B4b_T2h_I4b(_,_,_,_,_,_) => "P4h_N4b_G4b_B4b_T2h_I4b",
+        };        
+        f.debug_struct("RDModell")
+         .field("meta", &self.meta)
+         .field("vertex_format", &vformat)
+         .field("vertex_offset", &self.vertex_offset)
+         .field("vertices_count", &self.vertices_count)
+         .field("vertex_buffer_size", &self.vertex_buffer_size)
+         .field("triangles_offset", &self.triangles_offset)
+         .field("triangles_idx_count", &self.triangles_idx_count)
+         .field("triangles_idx_size", &self.triangles_idx_size)
+         .finish()
+        
+    }
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct Triangle {
+    indices: [u16;3]
+}
+
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct P4h {
+    pos: [f16; 4],
+}
+#[derive(Debug)]
+#[repr(C)]
+pub struct N4b {
+    normals: [u8; 4],
+}
+#[derive(Debug)]
+#[repr(C)]
+pub struct G4b {
+    tangent: [u8; 4],
+}
+#[derive(Debug)]
+#[repr(C)]
+pub struct B4b {
+    binormal: [u8; 4],
+}
+#[derive(Debug)]
+#[repr(C)]
+pub struct T2h {
+    tex: [f16; 2],
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct I4b {
+    blend_idx: [u8; 4],
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct C4c {
+    unknown: [u8; 4],
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum VertexFormat {
+    P4h(P4h),
+    P4h_N4b_T2h_C4c(P4h, N4b, T2h,C4c),
+    P4h_N4b_G4b_B4b_T2h(P4h, N4b, G4b, B4b, T2h),
+    P4h_N4b_G4b_B4b_T2h_I4b(P4h, N4b, G4b, B4b, T2h, I4b),
+}
+
+impl VertexFormat {
+    fn get_p4h(&self) -> &P4h {
+        let p4h = match self {
+            VertexFormat::P4h(p4h) => p4h,
+            VertexFormat::P4h_N4b_T2h_C4c(p4h,_,_,_) => p4h,
+            VertexFormat::P4h_N4b_G4b_B4b_T2h(p4h,_,_,_,_) => p4h,
+            VertexFormat::P4h_N4b_G4b_B4b_T2h_I4b(p4h,_,_,_,_,_) => p4h,
+        };    
+        p4h
+    }
+}
+
+struct VertexFormatSize;
+
+#[allow(non_upper_case_globals)]
+impl VertexFormatSize {
+    const P4h: u32 = 8;
+    const P4h_N4b_T2h_C4c: u32 = 20;
+    const P4h_N4b_G4b_B4b_T2h: u32 = 24;
+    const P4h_N4b_G4b_B4b_T2h_I4b: u32 = 28;
+}
+
+
+impl From<&Path> for RDModell {
+    fn from(f_path: &Path) -> Self {
+        let mut f = File::open(f_path).unwrap();
+        let mut buffer = Vec::new();
+        std::io::Read::read_to_end(&mut f, &mut buffer).ok();
+
+        let buffer_len = buffer.len();
+        info!("loaded {:?} into buffer", f_path.to_str().unwrap());
+        
+        info!("buffer size: {}", buffer_len);
+        let rdm = RDModell::new(buffer);
+        rdm
+    }
+}
+
+impl From<&str> for RDModell {
+    fn from(str_path: &str) -> Self {
+        RDModell::from(Path::new(str_path))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fishery_others_lod2() {
+        let rdm = RDModell::from("fishery_others_lod2.rdm");
+        assert_eq!(rdm.vertices_count,3291);
+        assert_eq!(rdm.triangles_idx_count,7473);
+
+        assert_eq!(rdm.triangles_idx_count as usize,rdm.triangle_indices.len()*3);
+    }
+
+    #[test]
+    fn basalt_crusher_others_lod2() {
+        let rdm = RDModell::from("basalt_crusher_others_lod2.rdm");
+        assert_eq!(rdm.vertices_count,2615);
+
+        assert_eq!(rdm.triangles_idx_count as usize,rdm.triangle_indices.len()*3);
+
+    }
+
+    #[test]
+    fn fishery_others_cutout_lod0() {
+        let rdm = RDModell::from("fishery_others_cutout_lod0.rdm");
+        assert_eq!(rdm.vertices_count,32);
+        assert_eq!(rdm.triangles_idx_count,78);
+
+        assert_eq!(rdm.triangles_idx_count as usize,rdm.triangle_indices.len()*3);
+    }
+
+    #[test]
+    fn ark_waterfall2() {
+        let rdm = RDModell::from("ark_waterfall2.rdm");
+        assert_eq!(rdm.vertices_count,105);
+
+        assert_eq!(rdm.triangles_idx_count as usize,rdm.triangle_indices.len()*3);
+    }
+
+    
+}
+
+
+fn main() {
+    env_logger::init();
+    info!("init !");
+
+    let rdm = RDModell::from("basalt_crusher_others_lod2.rdm");
+    //info!("rdm: {:#?}", rdm);
+
+    gltf_export::export(rdm);
+}
